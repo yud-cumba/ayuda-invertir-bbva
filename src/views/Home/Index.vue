6cb2fdb2d560dbd2ee44fa7721203f59dc66f72f
yud-cumba <yudith.cumba@gmail.com>
2021-10-22 18:01:55 -0500

    feat: add router to main layout

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,20 +1,16 @@
 <template>
 <div>
     <h1>HACKATON BBVA :v</h1>
-    <!-- <CardProduct/> -->
 </div>
 </template>
 
 <script>
-// import CardProduct from '@/components/CardProduct.vue';
-
 export default {
   name: 'Home',
   async created() {
     this.$store.commit('SET_LAYOUT', 'main-layout');
   },
-  // components: {
-  //   CardProduct,
-  // },
+  components: {
+  },
 };
 </script>

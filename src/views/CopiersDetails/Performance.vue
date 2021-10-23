2ecdada380cc67bee6070727f3edcfb11fef4a4d
omairapalacios <cpalaciosanchez@hotmail.com>
2021-10-23 11:01:24 -0500

    add copier detail view

---
+++ src/views/CopiersDetails/Performance.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <h4 class="my-3">Performance</h4>
+      <v-select
+          :items="items"
+          :value="items[0]"
+          color="indigo darken-2 my-0 py-0 rounded-xl"
+          solo
+          dense
+          outlined
+        ></v-select>
+      <Barchart class="chart my-0 py-0"/>
+    </div>
+</template>
+
+<script>
+import Barchart from '@/components/BarChart.vue';
+
+export default {
+  components: {
+    Barchart,
+  },
+  data() {
+    return {
+      items: ['2021', '2020'],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.chart {
+  height: 100px;
+  width: '100%';
+  position: 'relative';
+}
+</style>

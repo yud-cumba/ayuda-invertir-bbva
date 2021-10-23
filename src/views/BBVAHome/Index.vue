da45f4c0424ddd21004c5fb209cc2b0e721e1b4f
yud-cumba <yudith.cumba@gmail.com>
2021-10-23 11:48:50 -0500

    feat: add mosck bbva index

---
+++ src/views/BBVAHome/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Resume/>
+    </div>
+</template>
+
+<script>
+import Resume from './Resume.vue';
+
+export default {
+  components: { Resume },
+  created() {
+    this.$store.commit('SET_LAYOUT', 'bbva-layout');
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/BBVAHome/Resume.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="secondary white-text pa-3">
+        <v-row class="white--text d-flex px-3 justify-space-between">
+            <p >01 oct. - 01 nov.</p>
+            <p>Tendencias</p>
+        </v-row>
+        <h4 class="white--text mt-4">Ingresos $1032.34</h4>
+        <v-progress-linear
+        v-model="power"
+        color="amber"
+        height="15"
+        ></v-progress-linear>
+         <h4 class="white--text mt-4">Ingresos $1032.34</h4>
+         <v-progress-linear
+      v-model="power"
+      color="cyan"
+      height="15"
+    ></v-progress-linear>
+    </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    power: 78,
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

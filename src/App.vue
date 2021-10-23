ebb04f63a4e52b2956bfddd91a1866e6f8268c5f
yud-cumba <yudith.cumba@gmail.com>
2021-10-23 18:49:30 -0500

    feat: add no desktop

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <component :is="layout"></component>
+<div>
+  <NoDesktop v-if="!$vuetify.breakpoint.smAndDown"/>
+  <component v-else :is="layout"></component>
+</div>
 </template>
 <script>
 
@@ -7,9 +10,12 @@ import { mapState } from 'vuex';
 import LoginLayout from '@/layouts/Login.vue';
 import MainLayout from '@/layouts/Main.vue';
 import BbvaLayout from '@/layouts/Bbva.vue';
+import NoDesktop from '@/components/NoDesktop.vue';
 
 export default {
-  components: { MainLayout, BbvaLayout, LoginLayout },
+  components: {
+    MainLayout, BbvaLayout, LoginLayout, NoDesktop,
+  },
   computed: mapState(['layout']),
 };
 </script>

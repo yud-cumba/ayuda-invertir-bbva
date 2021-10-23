14a442256f92f6c66106c099cb52c7fec81a9f18
yud-cumba <yudith.cumba@gmail.com>
2021-10-22 22:48:25 -0500

    fix: move nabvar to file

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -15,16 +15,5 @@ import NavBar from '../components/NavBar.vue';
 
 export default {
   components: { NavBar },
-  data() {
-    return {
-      drawer: false,
-      group: null,
-    };
-  },
-  watch: {
-    group() {
-      this.drawer = false;
-    },
-  },
 };
 </script>

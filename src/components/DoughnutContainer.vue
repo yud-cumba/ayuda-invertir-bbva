98faf91386ca984503be72a53dc7dfb74f69d3c6
yud-cumba <yudith.cumba@gmail.com>
2021-10-23 15:50:07 -0500

    feat: add doughnut to expensive

---
+++ src/components/DoughnutContainer.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="px-5">
+  <doughnut
+    v-if="data.labels.length > 0"
+    :names="data.labels"
+    :chartdata="[{
+      data: data.values
+    }]"
+  />
+</div>
+</template>
+
+<script>
+import Doughnut from './Doughnut.vue';
+
+export default {
+  name: 'doughnut-container',
+  props: ['dataField', 'aggregations'],
+  computed: {
+    data() {
+      const { dataField, aggregations } = this;
+      const labels = [];
+      const values = [];
+      if (
+        aggregations
+        && aggregations[dataField]
+        && aggregations[dataField].buckets
+      ) {
+        aggregations[dataField].buckets.forEach(
+          // eslint-disable-next-line camelcase
+          ({ key: itemKey, doc_count }) => {
+            labels.push(itemKey);
+            values.push(doc_count);
+          },
+        );
+      }
+      return { labels, values };
+    },
+  },
+  components: {
+    Doughnut,
+  },
+};
+</script>

2948cf2b02bb9a879d914fe97f104041bb564644
yud-cumba <yudith.cumba@gmail.com>
2021-10-23 10:00:03 -0500

    feat: add dreams

--- src/views/Dreams/Index.vue
+++ src/views/Dreams/Index.vue
@@ -1,14 +1,50 @@
 <template>
     <div>
         <CardLearn/>
+        <v-col>
+            <CardDream v-for="dream in dreams" :dream="dream" :key="dream.id"/>
+        </v-col>
     </div>
 </template>
 
 <script>
 import CardLearn from '@/components/CardLearn.vue';
+import CardDream from './CardDream.vue';
 
 export default {
-  components: { CardLearn },
+  components: { CardLearn, CardDream },
+  data: () => ({
+    dreams: [
+      {
+        id: 'dream001',
+        name: 'Yuditd',
+        title: 'Viaje a Nueva Zelanda',
+        detail: 'Cumplí mi sueño invirtiendo en fondos mutos',
+        time: 12,
+      },
+      {
+        id: 'dream002',
+        name: 'Yuditd',
+        title: 'Viaje a Nueva Zelanda',
+        detail: 'Cumplí mi sueño invirtiendo en fondos mutos',
+        time: 12,
+      },
+      {
+        id: 'dream003',
+        name: 'Yuditd',
+        title: 'Viaje a Nueva Zelanda',
+        detail: 'Cumplí mi sueño invirtiendo en fondos mutos',
+        time: 12,
+      },
+      {
+        id: 'dream001',
+        name: 'Yuditd',
+        title: 'Viaje a Nueva Zelanda',
+        detail: 'Cumplí mi sueño invirtiendo en fondos mutos',
+        time: 12,
+      },
+    ],
+  }),
   created() {
     this.$store.commit('SET_LAYOUT', 'main-layout');
   },

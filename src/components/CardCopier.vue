87389ea80110d62236867b207a02bb1b6bf0dd2f
omairapalacios <cpalaciosanchez@hotmail.com>
2021-10-22 23:11:52 -0500

    edit data mock

--- src/components/CardCopier.vue
+++ src/components/CardCopier.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card color="primary" class="mx-auto pa-1 rounded-lg" max-width="100%">
+  <v-card color="primary" class="mx-auto py-1 rounded-lg" max-width="100%">
     <v-list-item three-line>
         <v-list-item-avatar class="avatar" height="100" width="125" tile color="grey">
           <v-img cover src="https://randomuser.me/api/portraits/women/76.jpg"></v-img>
@@ -14,7 +14,7 @@
        <v-row class="d-flex text-center text-center align-items-center px-4 mt-3">
          <v-icon class="ma-0 pa-0 icon" color="secondary">mdi-information-outline</v-icon>
          <p class="caption white--text mx-1">Riesgo</p>
-         <small class="px-2 pt-1 risk">6</small>
+         <small class="px-2 pt-1 risk">M</small>
        </v-row>
       </v-list-item-content>
     </v-list-item>

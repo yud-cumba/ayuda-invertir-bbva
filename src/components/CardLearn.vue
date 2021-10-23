a921f09263b65a10454657fb5db30f67f6bd6900
yud-cumba <yudith.cumba@gmail.com>
2021-10-22 22:11:24 -0500

    feat: changes to news

--- src/components/CardLearn.vue
+++ src/components/CardLearn.vue
@@ -1,20 +1,17 @@
 <template>
-<v-container>
-      <v-row dense>
-        <v-col cols="12">
           <v-card
-            color="#252A4D"
-            dark
+            class="primary white--text pa-2"
           >
-            <v-card-title class="text-h5">
+            <v-card-title class="text-h5 white--text">
               Aprender de los expertos
             </v-card-title>
 
-            <v-card-subtitle>En BBVA aprendemos y crecemos contigo ven a conocer con nosotros</v-card-subtitle>
+            <v-card-subtitle class="white--text">
+              En BBVA aprendemos y crecemos contigo ven a conocer con nosotros</v-card-subtitle>
 
             <v-card-actions>
               <v-btn
-              class="mt-2"
+              class="white--text mt-2"
                     outlined
                     rounded
                     small>
@@ -22,68 +19,6 @@
               </v-btn>
             </v-card-actions>
           </v-card>
-        </v-col>
-
-        <v-col
-          v-for="(item, i) in items"
-          :key="i"
-          cols="12"
-        >
-          <v-card
-            :color="item.color"
-            dark
-          >
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title
-                  class="text-h5"
-                  v-text="item.title"
-                ></v-card-title>
-
-                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-
-                <v-card-actions>
-                  <v-btn
-                    v-if="item.artist === 'Ellie Goulding'"
-                    class="ml-2 mt-3"
-                    fab
-                    icon
-                    height="40px"
-                    right
-                    width="40px"
-                  >
-                    <v-icon>mdi-play</v-icon>
-                  </v-btn>
-
-                  <v-btn
-                    v-else
-                    class="ml-2 mt-5"
-                    outlined
-                    rounded
-                    small
-                  >
-                    START RADIO
-                  </v-btn>
-                </v-card-actions>
-              </div>
-
-              <v-avatar
-                class="ma-3"
-                size="125"
-                tile
-              >
-                <v-img :src="item.src"></v-img>
-              </v-avatar>
-            </div>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-    <!-- <v-card
-    class="primary white--text"
-    >
-    holaaa
-    </v-card> -->
 </template>
 
 <script>

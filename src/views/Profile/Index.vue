576fcbedef96e93e57563ef7a8075d925c42bed3
yud-cumba <yudith.cumba@gmail.com>
2021-10-23 21:48:15 -0500

    feat: add profile page again

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -1,79 +1,61 @@
 <template>
-  <div>
-    <v-img width="300px" src="../../assets/saving.jpeg"></v-img>
-    <span>Tus ahorros son:</span>
-    <h1>$12,785</h1>
-    <template>
-      <v-tab-item>
-        <v-card>
-          <v-toolbar flat color="primary" dark>
-            <v-toolbar-title>Te contamos de nuestros tipos de inversiones</v-toolbar-title>
-          </v-toolbar>
-          <v-tabs vertical>
-            <v-tab>
-              Conservador
-            </v-tab>
-            <v-tab>
-              Moderado
-            </v-tab>
-            <v-tab>
-              Avanzado
-            </v-tab>
-            <v-tab-item>
-              <v-card flat>
-                <v-card-text>
-                  <template>
-                    <v-card class="mx-auto my-12" max-width="300">
-                      <v-img
-                        height="250"
-                        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                      ></v-img>
-                      <v-card-text>
-                        <v-row align="center" class="mx-0">
-                          <div class="my-4 text-subtitle-1">
-                            Tu ahorro
-                            <span>12000</span>
-                          </div>
-                          <div class="grey--text ms-4">
-                            Ganancia
-                            <span>789.6</span>
-                          </div>
-                        </v-row>
-
-                        <div class="my-4 text-subtitle-1">
-                          Tiempo
-                          <span>3 meses</span>
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </template>
-                                  </v-card-text></v-card
-              ></v-tab-item
-            ></v-tabs
-          ></v-card
-        ></v-tab-item
-      ></template
-    >
+  <div class="d-flex flex-column align-center justify-center">
+    <v-img width="200" src="@/assets/saving.jpeg"></v-img>
+    <span class="text-center mt-2">Tus ahorros son:</span>
+    <h1 class="text-center">$12,785</h1>
+    <p class="font-weight-bold">Te contamos de nuestros tipos de inversiones</p>
+    <div class="container">
+      <v-card v-for="type in types" :key="type.title"
+      width="150" class="elevation-0 mx-4 d-flex flex-column align-center justify-center" height="200">
+      <h4>{{type.title}}</h4>
+        <v-img width="100" max-height="100" src="@/assets/saving.jpeg" class="rounded-lg"></v-img>
+        <h4>{{type.save}}</h4>
+        <h4 class="teal--text">{{type.gain}}</h4>
+        <h4> {{type.time}} meses</h4>
+      </v-card>
+</div>
   </div>
 </template>
 
 <script>
 export default {
-
+  created() {
+    this.$store.commit('SET_LAYOUT', 'main-layout');
+  },
+  data: () => ({
+    types: [
+      {
+        title: 'Conservador',
+        img: '',
+        save: 12000,
+        gain: 789.6,
+        time: 3,
+      },
+      {
+        title: 'Moderado',
+        img: '',
+        save: 12000,
+        gain: 789.6,
+        time: 3,
+      },
+      {
+        title: 'Avanzado',
+        img: '',
+        save: 12000,
+        gain: 789.6,
+        time: 3,
+      },
+    ],
+  }),
 };
 </script>
 
 <style lang="scss" scoped>
-.text-card{
-  position: absolute;
-    width: 300px;
-    height: 18px;
-    left: 37px;
-    top: 374px;
-    font-family: "Euclid Circular A";
-    font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 18px;
+.container {
+    display: flex;
+    overflow-x: auto;
+}
+.container::-webkit-scrollbar {
+    display: none;
 }
 </style>

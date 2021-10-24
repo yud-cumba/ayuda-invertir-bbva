f96936f9ba764cc445c878c6a84ac8ee45a74e15
sandymarmolejo <sandymarmolejoc@gmail.com>
2021-10-23 23:53:48 -0500

    images profiles

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -5,10 +5,19 @@
     <h1 class="text-center">$12,785</h1>
     <p class="mx-2 text-center font-weight-bold">Te contamos de nuestros tipos de inversiones</p>
     <div class="container">
+      <v-card
+        flat
+       class="flat mx-2 d-flex flex-column align-center justify-center" height="200" width="300"
+      >
+      <br><br><br><br><br>
+        <h5>Tu Ahorro</h5>
+        <h5>Ganancia</h5>
+        <h5>Tiempo</h5>
+     </v-card>
       <v-card v-for="type in types" :key="type.title"
       width="150" class="elevation-0 mx-4 d-flex flex-column align-center justify-center" height="200">
       <h4>{{type.title}}</h4>
-        <v-img width="100" max-height="100" src="@/assets/saving.jpeg" class="rounded-lg"></v-img>
+        <v-img width="100" max-height="100" src="@/assets/conservative-profile.png" class="rounded-lg"></v-img>
         <h4>{{type.save}}</h4>
         <h4 class="teal--text">{{type.gain}}</h4>
         <h4> {{type.time}} meses</h4>
@@ -29,7 +38,7 @@ export default {
     types: [
       {
         title: 'Conservador',
-        img: '',
+        src: '@/assets/conservative-profile.png',
         save: 12000,
         gain: 789.6,
         time: 3,

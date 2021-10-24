9e37690c122b32c3c53cfa0a5c033b881b7437ca
yud-cumba <yudith.cumba@gmail.com>
2021-10-23 23:59:59 -0500

    feat: add percentage page

--- src/views/Dreams/CardDream.vue
+++ src/views/Dreams/CardDream.vue
@@ -2,7 +2,9 @@
        <v-card  class="mx-auto pa-0 ma-0 elevation-0" max-width="100%">
     <v-list-item three-line>
         <v-list-item-avatar class="avatar" height="130" width="125" tile color="grey">
-          <v-img cover src="https://randomuser.me/api/portraits/women/23.jpg"></v-img>
+          <v-img cover
+            :src="img">
+             </v-img>
         </v-list-item-avatar>
       <v-list-item-content class="mt-3">
           <h3 class="font-weight-regular mb-1">{{dream.name}}</h3>
@@ -15,7 +17,12 @@
 </template>
 
 <script>
+/* eslint-disable max-len */
+
 export default {
+  data: () => ({
+    img: 'https://pixabay.com/get/g332e92b9546afc7facbda1b5156d8abaf4ce77a80e1abc2ae0abcb3d40be03a992e8e619b956d5a224eea537f3e191e5_1280.jpg',
+  }),
   props: {
     dream: {
       type: Object,
